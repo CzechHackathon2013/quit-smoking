9c2988feecaa0d2a104ee507596ec2b560051d6d
lukebe <luke.benda@gmail.com>
2013-10-19 20:35:33 +0200

    widget

--- QuitSmoking/src/main/java/cz/czechhackathon/QSWidgetProvider.java
+++ QuitSmoking/src/main/java/cz/czechhackathon/QSWidgetProvider.java
@@ -1,7 +1,50 @@
 package cz.czechhackathon;
 
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+import java.util.Random;
+
 /**
  * Created by Develop on 19.10.13.
  */
-public class QSWidgetProvider {
+public class QSWidgetProvider extends AppWidgetProvider {
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager,
+                         int[] appWidgetIds) {
+
+        // Get all ids
+        ComponentName thisWidget = new ComponentName(context,
+                QSWidgetProvider.class);
+        int[] allWidgetIds = appWidgetManager.getAppWidgetIds(thisWidget);
+        for (int widgetId : allWidgetIds) {
+
+            // create some random data
+            int number = (new Random().nextInt(100));
+
+            RemoteViews remoteViews = new RemoteViews(context.getPackageName(),
+                    R.layout.activity_widget);
+
+            // Set the text
+            remoteViews.setTextViewText(R.id.widget_textview_endurance, String.valueOf(number));
+
+            // Register an onClickListener
+            Intent intent = new Intent(context, QSWidgetProvider.class);
+
+            intent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+            intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, appWidgetIds);
+
+            PendingIntent pendingIntent = PendingIntent.getBroadcast(context,
+                    0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+            remoteViews.setOnClickPendingIntent(R.id.widget_button_cravings, pendingIntent);
+            appWidgetManager.updateAppWidget(widgetId, remoteViews);
+        }
+    }
+
 }
---
+++ QuitSmoking/src/main/java/cz/czechhackathon/WidgetUpdateHandler.java
@@ -0,0 +1,13 @@
+package cz.czechhackathon;
+
+/**
+ * Created by Develop on 19.10.13.
+ */
+public interface WidgetUpdateHandler {
+
+    public String getEndurance();
+    public String getSavings();
+    public void onCravingsClicked();
+    public void onFailedClicked();
+
+}
